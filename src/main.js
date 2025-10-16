7fd4f7c775b11ad001608e90d6d8f09b66496f3a
Yufan Liu <853464519@qq.com>
2025-10-16 13:44:58 +1100

    111

--- src/main.js
+++ src/main.js
@@ -1,18 +1,16 @@
 // src/main.js
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-import router from './router'
 import App from './App.vue'
+import router from './router'
 import { useAuth } from '@/stores/auth'
 
-console.log('ENV =', import.meta.env)
-console.log('API_BASE =', import.meta.env.VITE_API_BASE)
-
 const app = createApp(App)
-app.use(createPinia())
-app.use(router)
+const pinia = createPinia()
 
-const authStore = useAuth()
-authStore.bootstrap().finally(() => {
-  app.mount('#app')
-})
+app.use(pinia).use(router)
+
+// ✅ 调用 init，而不是 bootstrap
+useAuth().init()
+
+app.mount('#app')
